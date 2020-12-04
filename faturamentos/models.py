077d1b8b34d56be5f47faa2f2fa9b15e31038f97
OswaldoSoares <oswaldosoares@gmail.com>
2020-12-03 22:01:05 -0300

    BUG Migrate - Banco de dados - class Fatura - field VencimentoFatura - removido

--- faturamentos/models.py
+++ faturamentos/models.py
@@ -7,7 +7,7 @@ class Fatura(models.Model):
     DataFatura = models.DateField(default=0)
     ValorFatura = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     StatusFatura = models.CharField(max_length=6, default='ABERTA')
-    VencimentoFatura = models.DateField(default=0)
+    # VencimentoFatura = models.DateField(default=0)
     ValorPagamento = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     Comentario = models.TextField(blank=True, null=True)
 
