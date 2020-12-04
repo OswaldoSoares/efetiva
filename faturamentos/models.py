d86d12501bcc3b169ee757bf035f2bea8b0a0fe7
OswaldoSoares <oswaldosoares@gmail.com>
2020-12-03 22:30:05 -0300

    BUG Migrate - Banco de dados - class Fatura - field DataPagamento - adicionado

--- faturamentos/models.py
+++ faturamentos/models.py
@@ -6,9 +6,10 @@ class Fatura(models.Model):
     Fatura = models.IntegerField()
     DataFatura = models.DateField(default=0)
     ValorFatura = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    StatusFatura = models.CharField(max_length=6, default='ABERTA')
     VencimentoFatura = models.DateField(default='2020-01-01')
+    StatusFatura = models.CharField(max_length=6, default='ABERTA')
     ValorPagamento = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    DataPagamento = models.DateField(default='2020-01-01')
     Comentario = models.TextField(blank=True, null=True)
 
     class Meta:
