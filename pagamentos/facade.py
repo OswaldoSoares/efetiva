7de2bc1102413e1d64fae0ccd2ac43f830f27db9
OswaldoSoares <oswaldosoares@gmail.com>
2021-06-18 19:20:27 -0300

    Módulo Pagamentos: bug gera folha pagamento

--- pagamentos/facade.py
+++ pagamentos/facade.py
@@ -64,7 +64,7 @@ def create_context_formcontracheque():
     contrac = ContraCheque.objects.all()
     contrai = ContraChequeItens.objects.all()
     formcontracheque = CadastraContraCheque()
-    contexto = {'formcontracheque': formcontracheque, 'bdcartao': bdcartao}
+    contexto = {'formcontracheque': formcontracheque, 'contrac': contrac}
     return contexto
 
 
