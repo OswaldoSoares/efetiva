390e1e69e5f325fb48c988e04a2ca3f38c8c9c20
OswaldoSoares <oswaldosoares@gmail.com>
2021-08-07 15:48:03 -0300

    Módulo Minutas: Test - Falha deliberada

--- minutas/tests/test_views.py
+++ minutas/tests/test_views.py
@@ -1,22 +1,7 @@
-from selenium import webdriver
-import unittest
+from django.test import TestCase
 
+class InicialTeste(TestCase):
 
-class AcessaModuloMinutas(unittest.TestCase):
+    def test_bad_soma(self):
+        self.assertEqual(1 + 1, 3)
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def test_verifica_pagina_inicial_modulo_miutas(self):
-        self.browser.get('http://localhost:8000/minutas')
-
-        # Título da pagina menciona a palavra Modulo
-        self.assertIn('Módulo', self.browser.title)
-        self.fail('Teste Finalizado!')
-
-
-if __name__ == '__main__':
-    unittest.main()
