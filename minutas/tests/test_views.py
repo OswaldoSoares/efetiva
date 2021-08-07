83ce38580100dfd932be6e9b6bea817a29d6287a
OswaldoSoares <oswaldosoares@gmail.com>
2021-08-07 16:22:06 -0300

    Módulo Minutas: Test - Primeiro teste de unidade e mapeamento de URL

--- minutas/tests/test_views.py
+++ minutas/tests/test_views.py
@@ -1,7 +1,9 @@
+from django.urls import resolve
 from django.test import TestCase
+from minutas.views import index_minuta
 
-class InicialTeste(TestCase):
+class PaginaInicialMinutas(TestCase):
 
-    def test_bad_soma(self):
-        self.assertEqual(1 + 1, 3)
-
+    def test_root_url_resolve_to_pagina_inicial_minutas_view(self):
+        found = resolve('/minutas/')
+        self.assertEqual(found.func, index_minuta)
