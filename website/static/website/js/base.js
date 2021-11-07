fafff31159e6e2d36ee8becb7d542e640532722d
OswaldoSoares <oswaldosoares@gmail.com>
2021-11-07 06:54:20 -0300

    Menu responsivo

--- website/static/website/js/base.js
+++ website/static/website/js/base.js
@@ -1,2 +1,14 @@
 $(".div-sucesso").hide()
 $(".div-erro").hide()
+
+if ($(window).width() <= 800) {
+    $(".menu-nav-icons").css('display', 'none')
+    $(".menu-dots").css('display', 'block')
+    $(".container").css('margin-top', '0')
+} else {
+    $(".container").css('margin-top', '79px')
+}
+
+$(document).on('click', '#menu-dots', function(event) {
+    $(".menu-nav-icons").css('display', 'block')
+});
