89a3a9cb920e94097980cab7ea6e1da3ee56aace
OswaldoSoares <oswaldosoares@gmail.com>
2021-11-16 14:03:27 -0300

    production.py alterado

--- transefetiva/settings/production.py
+++ transefetiva/settings/production.py
@@ -16,7 +16,7 @@ DATABASES = {
 }
 
 STATIC_URL = '/static/'
-STATIS_ROOT = '/app/site/public/static'
+STATIC_ROOT = '/app/site/public/static'
 
 # SECURE_HSTS_SECONDS = 15780000
 # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
