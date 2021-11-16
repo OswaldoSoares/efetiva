d38e402ba12a565fe382a151b132030229e81682
OswaldoSoares <oswaldosoares@gmail.com>
2021-11-16 17:01:04 -0300

    settings.py alterado

--- transefetiva/settings/settings.py
+++ transefetiva/settings/settings.py
@@ -15,6 +15,7 @@ from django.conf.locale.pt_BR import formats as br_formats
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
