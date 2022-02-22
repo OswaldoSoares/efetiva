7e05e1a6bb9c2b7ab8dd19c1324555395f140424
OswaldoSoares <oswaldosoares@gmail.com>
2022-02-22 10:10:36 -0300

    Módulo WebSite: recriada class model FileUpload, error migrate

--- website/models.py
+++ website/models.py
@@ -19,5 +19,9 @@ class Parametros(models.Model):
     objects = models.Manager()
 
 
-
+class FileUpload(models.Model):
+    idFileUpload = models.AutoField(primary_key=True)
+    Descricao = models.CharField(max_length=50)
+    upLoadFile = models.FileField(upload_to='upload_files/')
+    DateUpload = models.DateTimeField(auto_now=True)
     
