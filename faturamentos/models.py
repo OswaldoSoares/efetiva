431a081475c4d1d2a880738627c5e0ba83a60a4b
OswaldoSoares <oswaldosoares@gmail.com>
2022-04-10 11:52:44 -0300

    Módulo Faturamentos: Criado model EmailEnviado

--- faturamentos/models.py
+++ faturamentos/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 
@@ -6,13 +8,25 @@ class Fatura(models.Model):
     Fatura = models.IntegerField()
     DataFatura = models.DateField(default=0)
     ValorFatura = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    VencimentoFatura = models.DateField(default='2020-01-01')
-    StatusFatura = models.CharField(max_length=6, default='ABERTA')
+    VencimentoFatura = models.DateField(default="2020-01-01")
+    StatusFatura = models.CharField(max_length=6, default="ABERTA")
     ValorPagamento = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    DataPagamento = models.DateField(default='2020-01-01')
+    DataPagamento = models.DateField(default="2020-01-01")
     Comentario = models.TextField(blank=True, null=True)
 
     class Meta:
-        db_table = 'fatura'
+        db_table = "fatura"
+
+    objects = models.Manager()
+
+
+class EmailEnviado(models.Model):
+    idEmailEnviado = models.AutoField(primary_key=True)
+    DataEnviado = models.DateField(default=datetime.now, blank=True)
+    EmailsEnviado = models.CharField(max_length=250)
+    MensagemAdicional = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = "email_enviado"
 
     objects = models.Manager()
