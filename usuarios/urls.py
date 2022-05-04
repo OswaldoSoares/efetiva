e499688d818cde154251aff4e589381a8cd4cc6a
OswaldoSoares <oswaldosoares@gmail.com>
2022-05-04 19:11:47 -0300

    Módulo Usuários: Tela de login e logout com novo estilo

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,13 +1,27 @@
-from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
-from .views import tela_logout, cadastra_usuario
+from django.urls import path, reverse_lazy
+
+from .forms import MyLoginForm
+from .views import cadastra_usuario, tela_logout
 
 urlpatterns = [
-    path('cadastrausuario', cadastra_usuario, name='cadastrausuario'),
-    path('login', auth_views.LoginView.as_view(template_name='usuarios/login.html', extra_context={
-        'titulo': 'LOGIN'}), name='login'),
-    path('logout', tela_logout, name='logout'),
-    path('sair', auth_views.LogoutView.as_view(), name='sair'),
-    path('senha', auth_views.PasswordChangeView.as_view(template_name='usuarios/login.html', extra_context={
-        'titulo': 'ALTERAR SENHA ATUAL'}, success_url=reverse_lazy('logout')), name='senha'),
+    path("cadastrausuario", cadastra_usuario, name="cadastrausuario"),
+    path(
+        "login",
+        auth_views.LoginView.as_view(
+            template_name="usuarios/login.html", authentication_form=MyLoginForm
+        ),
+        name="login",
+    ),
+    path("logout", tela_logout, name="logout"),
+    path("sair", auth_views.LogoutView.as_view(), name="sair"),
+    path(
+        "senha",
+        auth_views.PasswordChangeView.as_view(
+            template_name="usuarios/login.html",
+            extra_context={"titulo": "ALTERAR SENHA ATUAL"},
+            success_url=reverse_lazy("logout"),
+        ),
+        name="senha",
+    ),
 ]
