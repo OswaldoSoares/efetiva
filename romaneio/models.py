0092f067a210ca348bd10af0f3ed3c163546ed43
OswaldoSoares <oswaldosoares@gmail.com>
2022-07-27 13:06:56 -0300

    Módulo Romaneios: criada a class NotasClientes

--- romaneio/models.py
+++ romaneio/models.py
@@ -1,3 +1,41 @@
+from clientes.models import Cliente
 from django.db import models
 
-# Create your models here.
+
+class NotasClientes(models.Model):
+    idNotasClientes = models.AutoField(primary_key=True)
+    DataColeta = models.DateField(default=0)
+    LocalColeta = models.CharField(max_length=200)
+    NumeroNota = models.CharField(max_length=15, default=0)
+    Destinatario = models.CharField(max_length=200, blank=True, null=True)
+    Endereco = models.CharField(max_length=200, blank=True, null=True)
+    Bairro = models.CharField(max_length=100, blank=True, null=True)
+    Cidade = models.CharField(max_length=30, default="SÃO PAULO")
+    Estado = models.CharField(max_length=2, default="SP")
+    Volume = models.IntegerField(default=0)
+    Peso = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    Valor = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    StatusNota = models.CharField(max_length=15)
+    Historico = models.TextField(default="", blank=True, null=True)
+    idCliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "notas_cliente"
+
+    def __str__(self):
+        return str(self.idNotasClientes)
+
+    def save(self, *args, **kwargs):
+        self.LocalColeta = self.LocalColeta.upper()
+        self.NumeroNota = self.NumeroNota.upper()
+        self.Descricao = self.Descricao.upper()
+        self.Endereco = self.Endereco.upper()
+        self.Bairro = self.Bairro.upper()
+        self.Cidade = self.Cidade.upper()
+        self.Estado = self.Estado.upper()
+        self.StatusNota = self.StatusNota.upper()
+        self.Historico = self.Historico.upper()
+
+        super(NotasClientes, self).save(*args, **kwargs)
+
+    objects = models.Manager()
