31ba9166b8b8433fe63e7533603a2e14400e4f24
OswaldoSoares <oswaldosoares@gmail.com>
2022-07-27 13:16:17 -0300

    Módulo Romaneios: Inclusão do app nas configurações

--- transefetiva/settings/settings.py
+++ transefetiva/settings/settings.py
@@ -1,64 +1,66 @@
 import os
+
 from decouple import config
 from django.conf.locale.pt_BR import formats as br_formats
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # apps de terceiros
-    'rolepermissions',
+    "rolepermissions",
     # meus apps
-    'clientes',
-    'despesas',
-    'faturamentos',
-    'minutas',
-    'orcamentos',
-    'pagamentos',
-    'pessoas',
-    'usuarios.apps.UsuariosConfig',
-    'veiculos',
-    'website',
+    "clientes",
+    "despesas",
+    "faturamentos",
+    "minutas",
+    "orcamentos",
+    "pagamentos",
+    "pessoas",
+    "romaneios",
+    "usuarios.apps.UsuariosConfig",
+    "veiculos",
+    "website",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'transefetiva.urls'
+ROOT_URLCONF = "transefetiva.urls"
 
-WSGI_APPLICATION = 'transefetiva.wsgi.application'
+WSGI_APPLICATION = "transefetiva.wsgi.application"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -67,15 +69,15 @@ USE_L10N = True
 USE_TZ = True
 
 # LOGIN
-LOGIN_REDIRECT_URL = 'index_website'
-LOGIN_URL = 'login'
-LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = "index_website"
+LOGIN_URL = "login"
+LOGOUT_REDIRECT_URL = "login"
 
-br_formats.DATE_FORMAT = 'd/m/Y'
+br_formats.DATE_FORMAT = "d/m/Y"
 
 # Permissões - DJANGO-ROLE-PERMISSIONS
-ROLEPERMISSIONS_MODULE = 'transefetiva.roles'
+ROLEPERMISSIONS_MODULE = "transefetiva.roles"
 ROLEPERMISSIONS_REGISTER_ADMIN = True
 # ROLEPERMISSIONS_REDIRECT_TO_LOGIN = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, config('STATIC_ROOT_CONFIG', ''))
+STATIC_ROOT = os.path.join(BASE_DIR, config("STATIC_ROOT_CONFIG", ""))
--- transefetiva/urls.py
+++ transefetiva/urls.py
@@ -13,33 +13,39 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from clientes import urls as clientes_urls
 from despesas import urls as despesas_urls
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 from faturamentos import urls as faturamentos_urls
 from minutas import urls as minutas_urls
 from orcamentos import urls as orcamentos_urls
 from pagamentos import urls as pagamentos_urls
 from pessoas import urls as pessoas_urls
+from romaneios import urls as romaneios_urls
 from usuarios import urls as usuarios_urls
 from veiculos import urls as veiculos_urls
 from website import urls as website_urls
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('clientes/', include(clientes_urls)),
-    path('despesas/', include(despesas_urls)),
-    path('faturamentos/', include(faturamentos_urls)),
-    path('minutas/', include(minutas_urls)),
-    path('orcamentos/', include(orcamentos_urls)),
-    path('pagamentos/', include(pagamentos_urls)),
-    path('pessoas/', include(pessoas_urls)),
-    path('usuarios/', include(usuarios_urls)),
-    path('veiculos/', include(veiculos_urls)),
-    path('', include(website_urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("clientes/", include(clientes_urls)),
+        path("despesas/", include(despesas_urls)),
+        path("faturamentos/", include(faturamentos_urls)),
+        path("minutas/", include(minutas_urls)),
+        path("orcamentos/", include(orcamentos_urls)),
+        path("pagamentos/", include(pagamentos_urls)),
+        path("pessoas/", include(pessoas_urls)),
+        path("romaneios/", include(romaneios_urls)),
+        path("usuarios/", include(usuarios_urls)),
+        path("veiculos/", include(veiculos_urls)),
+        path("", include(website_urls)),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
 
-handler403 = 'usuarios.views.my_403_template'
+handler403 = "usuarios.views.my_403_template"
