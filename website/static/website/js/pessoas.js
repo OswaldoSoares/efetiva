d64218e00d1ed67cc57124e83fff2742f894f42b
OswaldoSoares <oswaldosoares@gmail.com>
2022-11-18 21:00:20 -0300

    Módulo Pessoas: Criado arquivo pessoas.js e função js-seleciona-colaborador

---
+++ website/static/website/js/pessoas.js
@@ -0,0 +1,15 @@
+$(document).on('click', ".js-seleciona-colaborador", function() {
+    var id_pessoal = $(this).data("id_pessoal");
+    $.ajax({
+        type: "GET",
+        url: "/pessoas/consulta_pessoa",
+        data: {
+            id_pessoal: id_pessoal,
+        },
+        beforeSend: function() {},
+        success: function(data) {},
+        error: function(errorThrown) {
+            console.log("error: " + errorThrown)
+        }
+    });
+});
