28c13405c62ba65f5c1598821f1723750b54e7a9
OswaldoSoares <oswaldosoares@gmail.com>
2022-12-02 10:59:37 -0300

    Configura MEDIA_ROOT, development and production

--- transefetiva/settings/production.py
+++ transefetiva/settings/production.py
@@ -1,51 +1,51 @@
 from .settings import *
 
-with open('/efetiva/auth/secret_key.txt') as f:
+with open("/efetiva/auth/secret_key.txt") as f:
     SECRET_KEY = f.read().strip()
 
 DEBUG = True
-ALLOWED_HOSTS = ['137.184.201.225', 'www.transefetiva.com.br', 'transefetiva.com.br']
+ALLOWED_HOSTS = ["137.184.201.225", "www.transefetiva.com.br", "transefetiva.com.br"]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/efetiva/auth/mysql.cnf',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "/efetiva/auth/mysql.cnf",
         },
     },
 }
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            '/efetiva/templates',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            "/efetiva/templates",
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 # STATIC_ROOT = '/efetiva/site/public/static'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/efetiva/site/public/media'
+MEDIA_URL = "/media/"
+# MEDIA_ROOT = '/efetiva/site/public/media'
 
 # Configuração de E-Mails
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST = config('EMAIL_HOST', '')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST = config("EMAIL_HOST", "")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", "")
 
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
