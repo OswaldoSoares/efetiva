c81c04a3a022a957b53039f5c0b69048b670fff9
OswaldoSoares <oswaldosoares@gmail.com>
2022-12-02 13:52:08 -0300

    Módulo Pessoas: Botão para salvar foto

--- website/static/website/js/pessoas.js
+++ website/static/website/js/pessoas.js
@@ -61,9 +61,9 @@ $(document).on('click', ".js-salva-foto", function(event) {
     });
 });
 
-$(document).on('change', '.js-carrega-foto', function() {
-    $(".js-salva-foto").click()
-});
+// $(document).on('change', '.js-carrega-foto', function() {
+//     $(".js-salva-foto").click()
+// });
 
 $(document).on('click', '.js-atualiza-decimo-terceiro', function() {
     $.ajax({
