7fa2d36a485c176b52f70643be17e1074485b862
OswaldoSoares <oswaldosoares@gmail.com>
2023-10-09 11:11:50 -0300

    Teste de inclusão de recibo refatorado

--- pagamentos/tests/test_models.py
+++ pagamentos/tests/test_models.py
@@ -1,30 +1,23 @@
-# import pytest
-# from pagamentos.models import Recibo
-# from pessoas.models import Pessoal
+"""
+    Testes models do app pagamentos
+"""
+import pytest
+from pagamentos.models import Recibo
+from pessoas.models import Pessoal
 
 
-# def test_oi(client):
-#     response = client.get("/website/index.html")
-#     assert response.status_code == 200
-#     assert response.content == "OI MUNDO"
-
-
-# @pytest.mark.django_db
-# class TestRecibo:
-#     def test_recibo_create(self, client) -> None:
-#         assert Recibo.objects.count() == 0
-#         pessoa: Pessoal = Pessoal.objects.create(Nome="Oswaldo")
-#         response = client.post(
-#             "/pagamentos/criapagamento",
-#             {
-#                 "Recibo": 1500,
-#                 "DataRecibo": "13/04/2022",
-#                 "ValorRecibo": "10.00",
-#                 "StatusRecibo": "ABERTA",
-#                 "DataPagamento": "01/01/2020",
-#                 "Comentario": "Qualquer comentário",
-#                 "idPessoal": pessoa.idPessoal,
-#             },
-#         )
-#         assert response.status_code == 200, response.data
-#         assert Recibo.objects.count() == 1
+@pytest.mark.django_db
+def test_recibo_create():
+    """
+    Testa inclusão de um novo registro no db recibo
+    """
+    pessoa = Pessoal.objects.create(Nome="Oswaldo")
+    Recibo.objects.create(
+        Recibo=1500,
+        DataRecibo="2023-10-06",
+        ValorRecibo="10.00",
+        StatusRecibo="ABERTA",
+        DataPagamento="2023-10-06",
+        Comentario="Qualquer comentário",
+        idPessoal=pessoa,
+    )
