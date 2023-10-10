34160f75b65a452f6dc0181f447503ec2a1010eb
OswaldoSoares <oswaldosoares@gmail.com>
2023-10-10 08:41:13 -0300

    criado teste test_view_index_pagamento_existe

--- pagamentos/tests/test_views.py
+++ pagamentos/tests/test_views.py
@@ -1,1 +1,33 @@
 import pytest
+from django.contrib.auth.models import User
+from django.test import Client
+from django.urls import reverse
+from rolepermissions.checkers import has_permission
+
+
+@pytest.mark.django_db
+def test_view_index_pagamento_existe(cria_super_user):
+    url = reverse("index_pagamento")
+    client = Client()
+    #  user = User.objects.create_superuser(
+    #  username="testuser", password="testpassword"
+    #  )
+    client.login(username="testuser", password="testpassword")
+    has_perm = has_permission(cria_super_user, "modulo_pagamentos")
+    response = client.get(url)
+    assert response.status_code == 200 if has_perm else 403
+    print(response.status_code)
+
+
+@pytest.fixture
+def cria_super_user():
+    """
+        Cria um super usuário para usar nos testes que necessitam de um.
+    Returns:
+        user
+
+    """
+    user = User.objects.create_superuser(
+        username="testuser", password="testpassword"
+    )
+    return user
