0bd46257872677314c7ef795230093f511f81bac
OswaldoSoares <oswaldosoares@gmail.com>
2023-12-13 10:49:33 -0300

    Oculta cards ao entrar no modulo pagamentos ou selecionar um mes_ano

--- website/static/website/js/pagamentos.js
+++ website/static/website/js/pagamentos.js
@@ -19,6 +19,21 @@ $(document).on("click", ".js-seleciona-mes-ano", function(event) {
         },
         beforeSend: function() {
             localStorage.setItem("mes_ano", mes_ano)
+            $(".js-folha").html('');
+            $(".js-saldo").html('');
+            $(".js-adiantamento").html('')
+            $(".js-funcionario-pagamento").html('');
+            $(".js-cartao-ponto").html('');
+            $(".js-itens-contra-cheque").html('');
+            $(".js-contra-cheque").html('');
+            $(".js-cria-vales").html('')
+            $(".js-minutas-pagamento").html('')
+            $(".js-lista-vales").html('')
+            $(".js-files-pagamento").html('')
+            $(".js-agenda-pagamento").html('')
+            $(".js-itens-agenda-pagamento").html('');
+            $(".card-vales-colaborador").hide();
+
             $(".card-periodo-avulso").hide();
             $('.box-loader').show();
         },
@@ -33,6 +48,20 @@ $(document).on("click", ".js-seleciona-mes-ano", function(event) {
 
 // Seleciona mês e ano para pagamento de colaboradores mensalistas
 $(".select-mes-ano").change(function() {
+    $(".js-folha").html('');
+    $(".js-saldo").html('');
+    $(".js-adiantamento").html('')
+    $(".js-funcionario-pagamento").html('');
+    $(".js-cartao-ponto").html('');
+    $(".js-itens-contra-cheque").html('');
+    $(".js-contra-cheque").html('');
+    $(".js-cria-vales").html('')
+    $(".js-minutas-pagamento").html('')
+    $(".js-lista-vales").html('')
+    $(".js-files-pagamento").html('')
+    $(".js-agenda-pagamento").html('')
+    $(".js-itens-agenda-pagamento").html('');
+    $(".card-vales-colaborador").hide();
     $(".js-seleciona-mes-ano").removeClass("icofont-checked")
     $(".js-seleciona-mes-ano").addClass("icofont-square")
 });
