1be4da920d238352e8da586b159297fbe6b87f28
OswaldoSoares <oswaldosoares@gmail.com>
2024-01-31 10:20:23 -0300

    Formatação do código

--- clientes/facade.py
+++ clientes/facade.py
@@ -5,8 +5,17 @@ from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.template.loader import render_to_string
 
-from clientes.models import Cliente, FoneContatoCliente, EMailContatoCliente, Cobranca, Tabela, TabelaVeiculo, \
-    TabelaPerimetro, TabelaCapacidade
+from clientes.models import (
+    Cliente,
+    FoneContatoCliente,
+    EMailContatoCliente,
+    Cobranca,
+    Tabela,
+    TabelaVeiculo,
+    TabelaPerimetro,
+    TabelaCapacidade,
+)
+
 # TODO IMPORTAR FROM FACADE VEICULOS
 from veiculos.models import CategoriaVeiculo
 
@@ -26,22 +35,29 @@ def create_cliente_context(idcliente: int):
     tabela_perimetro_cliente = get_cliente_tabela_perimetro(idcliente)
     tabela_capacidade_cliente = get_cliente_tabela_capacidade(idcliente)
     list_categoria_cliente = list_categoria_tabela_veiculo_cliente(idcliente)
-    context = {'cliente': cliente, 'fone_cliente': fone_cliente, 'email_cliente': email_cliente,
-               'cobranca_cliente': cobranca_cliente, 'tabela_cliente': tabela_cliente,
-               'tabela_veiculo_cliente': tabela_veiculo_cliente, 'tabela_perimetro_cliente': tabela_perimetro_cliente,
-               'tabela_capacidade_cliente': tabela_capacidade_cliente, 'list_categoria_cliente': list_categoria_cliente}
+    context = {
+        "cliente": cliente,
+        "fone_cliente": fone_cliente,
+        "email_cliente": email_cliente,
+        "cobranca_cliente": cobranca_cliente,
+        "tabela_cliente": tabela_cliente,
+        "tabela_veiculo_cliente": tabela_veiculo_cliente,
+        "tabela_perimetro_cliente": tabela_perimetro_cliente,
+        "tabela_capacidade_cliente": tabela_capacidade_cliente,
+        "list_categoria_cliente": list_categoria_cliente,
+    }
     return context
 
 
 def create_cliente_filter_context(request):
-    meufiltrofantasia = request.GET.get('filtrofantasia', None)
-    meufiltronome = request.GET.get('filtronome', None)
+    meufiltrofantasia = request.GET.get("filtrofantasia", None)
+    meufiltronome = request.GET.get("filtronome", None)
     cliente = get_cliente_all()
     if meufiltrofantasia:
         cliente = cliente.filter(Fantasia__icontains=meufiltrofantasia)
     elif meufiltronome:
         cliente = cliente.filter(Nome__icontains=meufiltronome)
-    context = {'cliente': cliente}
+    context = {"cliente": cliente}
     return context
 
 
@@ -87,7 +103,9 @@ def get_cliente_email(idcliente: int):
     :param idcliente:
     :return: queryset
     """
-    emailcontatocliente = EMailContatoCliente.objects.filter(idCliente=idcliente)
+    emailcontatocliente = EMailContatoCliente.objects.filter(
+        idCliente=idcliente
+    )
     return emailcontatocliente
 
 
@@ -148,7 +166,9 @@ def list_categoria_tabela_veiculo_cliente(idcliente: int):
     :return: list of Categorias
     """
     veiculos_categoria = get_cliente_tabela_veiculo(idcliente)
-    veiculos_categoria_existe = [itens.idCategoriaVeiculo_id for itens in veiculos_categoria]
+    veiculos_categoria_existe = [
+        itens.idCategoriaVeiculo_id for itens in veiculos_categoria
+    ]
     return veiculos_categoria_existe
 
 
@@ -162,70 +182,107 @@ def create_select_cliente():
     Create a Select com id e fantasia
     :return:
     """
-    return [('0', 'SELECIONE UM ITEM')] + [(i.idCliente, i.Fantasia) for i in get_cliente_all()]
+    return [("0", "SELECIONE UM ITEM")] + [
+        (i.idCliente, i.Fantasia) for i in get_cliente_all()
+    ]
 
 
 def form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente):
     data = dict()
     c_instance = None
-    if c_view == 'edita_cliente' or c_view == 'exclui_cliente':
+    if c_view == "edita_cliente" or c_view == "exclui_cliente":
         if c_idobj:
             c_instance = Cliente.objects.get(idCliente=c_idobj)
-    elif c_view == 'cria_email_cliente' or c_view == 'edita_email_cliente':
+    elif c_view == "cria_email_cliente" or c_view == "edita_email_cliente":
         if c_idobj:
-            c_instance = EMailContatoCliente.objects.get(idEmailContatoCliente=c_idobj)
-    elif c_view == 'cria_fone_cliente' or c_view == 'edita_fone_cliente':
+            c_instance = EMailContatoCliente.objects.get(
+                idEmailContatoCliente=c_idobj
+            )
+    elif c_view == "cria_fone_cliente" or c_view == "edita_fone_cliente":
         if c_idobj:
-            c_instance = FoneContatoCliente.objects.get(idFoneContatoCliente=c_idobj)
-    elif c_view == 'cria_cobranca_cliente' or c_view == 'edita_cobranca_cliente':
+            c_instance = FoneContatoCliente.objects.get(
+                idFoneContatoCliente=c_idobj
+            )
+    elif (
+        c_view == "cria_cobranca_cliente" or c_view == "edita_cobranca_cliente"
+    ):
         if c_idobj:
             c_instance = Cobranca.objects.get(idCobranca=c_idobj)
-    elif c_view == 'cria_tabela_cliente' or c_view == 'edita_tabela_cliente':
+    elif c_view == "cria_tabela_cliente" or c_view == "edita_tabela_cliente":
         if c_idobj:
             c_instance = Tabela.objects.get(idTabela=c_idobj)
-    elif c_view == 'cria_tabela_veiculo' or c_view == 'edita_tabela_veiculo':
+    elif c_view == "cria_tabela_veiculo" or c_view == "edita_tabela_veiculo":
         if c_idobj:
             c_instance = TabelaVeiculo.objects.get(idTabelaVeiculo=c_idobj)
-    elif c_view == 'cria_tabela_capacidade' or c_view == 'edita_tabela_capacidade':
+    elif (
+        c_view == "cria_tabela_capacidade"
+        or c_view == "edita_tabela_capacidade"
+    ):
         if c_idobj:
-            c_instance = TabelaCapacidade.objects.get(idTabelaCapacidade=c_idobj)
-    elif c_view == 'cria_tabela_perimetro' or c_view == 'edita_tabela_perimetro':
+            c_instance = TabelaCapacidade.objects.get(
+                idTabelaCapacidade=c_idobj
+            )
+    elif (
+        c_view == "cria_tabela_perimetro" or c_view == "edita_tabela_perimetro"
+    ):
         if c_idobj:
             c_instance = TabelaPerimetro.objects.get(idTabelaPerimetro=c_idobj)
-    if request.method == 'POST':
-        if c_view == 'edita_tabela_capacidade':
+    if request.method == "POST":
+        if c_view == "edita_tabela_capacidade":
             request_copy = request.POST.copy()
-            request_copy['CapacidadeFinal'] = c_instance.CapacidadeFinal
+            request_copy["CapacidadeFinal"] = c_instance.CapacidadeFinal
             form = c_form(request_copy, instance=c_instance)
-        elif c_view == 'edita_tabela_perimetro':
+        elif c_view == "edita_tabela_perimetro":
             request_copy = request.POST.copy()
-            request_copy['PerimetroFinal'] = c_instance.PerimetroFinal
+            request_copy["PerimetroFinal"] = c_instance.PerimetroFinal
             form = c_form(request_copy, instance=c_instance)
         else:
             form = c_form(request.POST, instance=c_instance)
         if form.is_valid():
             save_id = form.save()
-            if c_view == 'cria_cliente' or c_view == 'edita_cliente':
-                data['save_id'] = save_id.idCliente
+            if c_view == "cria_cliente" or c_view == "edita_cliente":
+                data["save_id"] = save_id.idCliente
             else:
-                data['save_id'] = save_id.idCliente_id
+                data["save_id"] = save_id.idCliente_id
         else:
             pass
     else:
-        if c_view == 'cria_tabela_capacidade':
-            peso = TabelaCapacidade.objects.filter(idCliente=idcliente).aggregate(peso=Max('CapacidadeFinal'))
-            form = c_form(initial={'CapacidadeInicial': peso['peso']+1, 'CapacidadeFinal': peso['peso']+2})
-        elif c_view == 'cria_tabela_perimetro':
-            km = TabelaPerimetro.objects.filter(idCliente=idcliente).aggregate(km=Max('PerimetroFinal'))
-            if not km['km']:
-                km['km'] = 0
-            form = c_form(initial={'PerimetroInicial': km['km']+1, 'PerimetroFinal': km['km']+2})
+        if c_view == "cria_tabela_capacidade":
+            peso = TabelaCapacidade.objects.filter(
+                idCliente=idcliente
+            ).aggregate(peso=Max("CapacidadeFinal"))
+            form = c_form(
+                initial={
+                    "CapacidadeInicial": peso["peso"] + 1,
+                    "CapacidadeFinal": peso["peso"] + 2,
+                }
+            )
+        elif c_view == "cria_tabela_perimetro":
+            km = TabelaPerimetro.objects.filter(idCliente=idcliente).aggregate(
+                km=Max("PerimetroFinal")
+            )
+            if not km["km"]:
+                km["km"] = 0
+            form = c_form(
+                initial={
+                    "PerimetroInicial": km["km"] + 1,
+                    "PerimetroFinal": km["km"] + 2,
+                }
+            )
         else:
             form = c_form(instance=c_instance)
-    context = {'form': form, 'c_idobj': c_idobj, 'c_url': c_url, 'c_view': c_view, 'idcliente': idcliente,
-               'idcategoriaveiculo': request.GET.get('idcategoriaveiculo')}
-    data['html_form'] = render_to_string('clientes/formcliente.html', context, request=request)
-    data['c_view'] = c_view
+    context = {
+        "form": form,
+        "c_idobj": c_idobj,
+        "c_url": c_url,
+        "c_view": c_view,
+        "idcliente": idcliente,
+        "idcategoriaveiculo": request.GET.get("idcategoriaveiculo"),
+    }
+    data["html_form"] = render_to_string(
+        "clientes/formcliente.html", context, request=request
+    )
+    data["c_view"] = c_view
     c_return = JsonResponse(data)
     return c_return
 
@@ -233,37 +290,62 @@ def form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente):
 def form_exclui_cliente(request, c_idobj, c_url, c_view, idcliente):
     data = dict()
     c_queryset = None
-    if c_view == 'exclui_cliente':
+    if c_view == "exclui_cliente":
         c_queryset = Cliente.objects.get(idCliente=c_idobj)
-    elif c_view == 'exclui_email_cliente':
-        c_queryset = EMailContatoCliente.objects.get(idEmailContatoCliente=c_idobj)
-    elif c_view == 'exclui_fone_cliente':
-        c_queryset = FoneContatoCliente.objects.get(idFoneContatoCliente=c_idobj)
-    elif c_view == 'exclui_cobranca_cliente':
+    elif c_view == "exclui_email_cliente":
+        c_queryset = EMailContatoCliente.objects.get(
+            idEmailContatoCliente=c_idobj
+        )
+    elif c_view == "exclui_fone_cliente":
+        c_queryset = FoneContatoCliente.objects.get(
+            idFoneContatoCliente=c_idobj
+        )
+    elif c_view == "exclui_cobranca_cliente":
         c_queryset = Cobranca.objects.get(idCobranca=c_idobj)
-    elif c_view == 'exclui_tabela_capacidade':
+    elif c_view == "exclui_tabela_capacidade":
         c_queryset = TabelaCapacidade.objects.get(idTabelaCapacidade=c_idobj)
-    elif c_view == 'exclui_tabela_perimetro':
+    elif c_view == "exclui_tabela_perimetro":
         c_queryset = TabelaPerimetro.objects.get(idTabelaPerimetro=c_idobj)
     if request.method == "POST":
         c_queryset.delete()
-    context = {'c_url': c_url, 'c_view': c_view, 'c_queryset': c_queryset, 'idcliente': idcliente}
-    data['html_form'] = render_to_string('clientes/formcliente.html', context, request=request)
-    data['c_view'] = c_view
-    data['save_id'] = idcliente
+    context = {
+        "c_url": c_url,
+        "c_view": c_view,
+        "c_queryset": c_queryset,
+        "idcliente": idcliente,
+    }
+    data["html_form"] = render_to_string(
+        "clientes/formcliente.html", context, request=request
+    )
+    data["c_view"] = c_view
+    data["save_id"] = idcliente
     c_return = JsonResponse(data)
     return c_return
 
 
 def phkesc(switchdict):
-    phkesc = {'porcentagem-cobra': '0', 'hora-cobra': '0', 'kilometragem-cobra': '0', 'entrega-cobra': '0',
-              'saida-cobra': '0', 'capacidade-cobra': '0', 'entregakg-cobra': '0', 'entregavolume-cobra': '0',
-              'porcentagem-paga': '0', 'hora-paga': '0', 'kilometragem-paga': '0', 'entrega-paga': '0',
-              'saida-paga': '0', 'capacidade-paga': '0', 'entregakg-paga': '0', 'entregavolume-paga': '0'}
+    phkesc = {
+        "porcentagem-cobra": "0",
+        "hora-cobra": "0",
+        "kilometragem-cobra": "0",
+        "entrega-cobra": "0",
+        "saida-cobra": "0",
+        "capacidade-cobra": "0",
+        "entregakg-cobra": "0",
+        "entregavolume-cobra": "0",
+        "porcentagem-paga": "0",
+        "hora-paga": "0",
+        "kilometragem-paga": "0",
+        "entrega-paga": "0",
+        "saida-paga": "0",
+        "capacidade-paga": "0",
+        "entregakg-paga": "0",
+        "entregavolume-paga": "0",
+    }
     for itens in phkesc:
-        if itens in switchdict['switch']:
-            phkesc[itens] = '1'
-    valor_phkesc = ''
+        if itens in switchdict["switch"]:
+            phkesc[itens] = "1"
+    valor_phkesc = ""
     for itens in phkesc:
         valor_phkesc += phkesc[itens]
     phkesc_cobra = valor_phkesc[0:8]
@@ -286,11 +368,13 @@ def save_phkesc(idtabelacliente, phkesc_cobra, phkesc_paga):
     obj.idFormaPagamento = tabela.idFormaPagamento
     obj.idCliente = tabela.idCliente
     obj.save()
-    c_return = redirect('consultacliente', tabela.idCliente_id)
+    c_return = redirect("consultacliente", tabela.idCliente_id)
     return c_return
 
 
 def create_contexto_seleciona_cliente():
     clientes = Cliente.objects.all()
-    lista = [{"idcliente": x.idCliente, "fantasia": x.Fantasia} for x in clientes]
+    lista = [
+        {"idcliente": x.idCliente, "fantasia": x.Fantasia} for x in clientes
+    ]
     return lista
