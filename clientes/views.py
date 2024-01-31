24d528ac6b16ab86818390ff45b7b952edace54f
OswaldoSoares <oswaldosoares@gmail.com>
2024-01-31 10:01:01 -0300

    Formatação do código

--- clientes/views.py
+++ clientes/views.py
@@ -48,7 +48,9 @@ def cria_cliente(request):
     c_url = "/clientes/criacliente/"
     c_view = "cria_cliente"
     idcliente = None
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -58,7 +60,9 @@ def edita_cliente(request, idcliente):
     c_url = "/clientes/editacliente/{}/".format(c_idobj)
     c_view = "edita_cliente"
     idcliente = idcliente
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -67,7 +71,9 @@ def exclui_cliente(request, idcliente):
     c_url = "/clientes/excluicliente/{}/".format(c_idobj)
     c_view = "exclui_cliente"
     idcliente = idcliente
-    data = facade.form_exclui_cliente(request, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_exclui_cliente(
+        request, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -77,7 +83,9 @@ def cria_email_cliente(request):
     c_url = "/clientes/criaemailcliente/"
     c_view = "cria_email_cliente"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -87,7 +95,9 @@ def edita_email_cliente(request, idclienteemail):
     c_url = "/clientes/editaemailcliente/{}/".format(c_idobj)
     c_view = "edita_email_cliente"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -96,7 +106,9 @@ def exclui_email_cliente(request, idclienteemail):
     c_url = "/clientes/excluiemailcliente/{}/".format(c_idobj)
     c_view = "exclui_email_cliente"
     idcliente = request.POST.get("idCliente")
-    data = facade.form_exclui_cliente(request, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_exclui_cliente(
+        request, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -106,7 +118,9 @@ def cria_fone_cliente(request):
     c_url = "/clientes/criafonecliente/"
     c_view = "cria_fone_cliente"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -116,7 +130,9 @@ def edita_fone_cliente(request, idclientefone):
     c_url = "/clientes/editafonecliente/{}/".format(c_idobj)
     c_view = "edita_fone_cliente"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -125,7 +141,9 @@ def exclui_fone_cliente(request, idclientefone):
     c_url = "/clientes/excluifonecliente/{}/".format(c_idobj)
     c_view = "exclui_fone_cliente"
     idcliente = request.POST.get("idCliente")
-    data = facade.form_exclui_cliente(request, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_exclui_cliente(
+        request, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -135,7 +153,9 @@ def cria_cobranca_cliente(request):
     c_url = "/clientes/criacobrancacliente/"
     c_view = "cria_cobranca_cliente"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -145,7 +165,9 @@ def edita_cobranca_cliente(request, idcobrancacliente):
     c_url = "/clientes/editacobrancacliente/{}/".format(c_idobj)
     c_view = "edita_cobranca_cliente"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -154,7 +176,9 @@ def exclui_cobranca_cliente(request, idcobrancacliente):
     c_url = "/clientes/excluicobrancacliente/{}/".format(c_idobj)
     c_view = "exclui_cobranca_cliente"
     idcliente = request.POST.get("idCliente")
-    data = facade.form_exclui_cliente(request, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_exclui_cliente(
+        request, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -164,7 +188,9 @@ def cria_tabela_cliente(request):
     c_url = "/clientes/criatabelacliente/"
     c_view = "cria_tabela_cliente"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -174,7 +200,9 @@ def edita_tabela_cliente(request, idclientetabela):
     c_url = "/clientes/editatabelacliente/{}/".format(c_idobj)
     c_view = "edita_tabela_cliente"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -190,7 +218,9 @@ def cria_tabela_veiculo(request):
     c_url = "/clientes/criatabelaveiculo/"
     c_view = "cria_tabela_veiculo"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -200,7 +230,9 @@ def edita_tabela_veiculo(request, idtabelaveiculo):
     c_url = "/clientes/editatabelaveiculo/{}/".format(c_idobj)
     c_view = "edita_tabela_veiculo"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -210,7 +242,9 @@ def cria_tabela_capacidade(request):
     c_url = "/clientes/criatabelacapacidade/"
     c_view = "cria_tabela_capacidade"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -220,7 +254,9 @@ def edita_tabela_capacidade(request, idtabelacapacidade):
     c_url = "/clientes/editatabelacapacidade/{}/".format(c_idobj)
     c_view = "edita_tabela_capacidade"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -229,7 +265,9 @@ def exclui_tabela_capacidade(request, idtabelacapacidade):
     c_url = "/clientes/excluitabelacapacidade/{}/".format(c_idobj)
     c_view = "exclui_tabela_capacidade"
     idcliente = request.POST.get("idCliente")
-    data = facade.form_exclui_cliente(request, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_exclui_cliente(
+        request, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -239,7 +277,9 @@ def cria_tabela_perimetro(request):
     c_url = "/clientes/criatabelaperimetro/"
     c_view = "cria_tabela_perimetro"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -249,7 +289,9 @@ def criaformapgto(request):
         formapgto.save()
     else:
         formapgto = CadastraFormaPgto()
-    return render(request, "clientes/criaformapgto.html", {"formapgto": formapgto})
+    return render(
+        request, "clientes/criaformapgto.html", {"formapgto": formapgto}
+    )
 
 
 def edita_tabela_perimetro(request, idtabelaperimetro):
@@ -258,7 +300,9 @@ def edita_tabela_perimetro(request, idtabelaperimetro):
     c_url = "/clientes/editatabelaperimetro/{}/".format(c_idobj)
     c_view = "edita_tabela_perimetro"
     idcliente = request.GET.get("idcliente")
-    data = facade.form_cliente(request, c_form, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_cliente(
+        request, c_form, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
@@ -267,7 +311,9 @@ def exclui_tabela_perimetro(request, idtabelaperimetro):
     c_url = "/clientes/excluitabelaperimetro/{}/".format(c_idobj)
     c_view = "exclui_tabela_perimetro"
     idcliente = request.POST.get("idCliente")
-    data = facade.form_exclui_cliente(request, c_idobj, c_url, c_view, idcliente)
+    data = facade.form_exclui_cliente(
+        request, c_idobj, c_url, c_view, idcliente
+    )
     return data
 
 
