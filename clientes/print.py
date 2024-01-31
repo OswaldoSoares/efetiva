19b156bcaa1fd6d8a309b2132ee0160685d9affd
OswaldoSoares <oswaldosoares@gmail.com>
2024-01-31 09:36:11 -0300

    Criado arquivo print.py e adicionada a função que imprime a ficha cadastral dos clientes

---
+++ clientes/print.py
@@ -0,0 +1,24 @@
+"""
+    Módulo de Impressão
+"""
+from io import BytesIO
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
+from website.print import header
+
+
+def ficha_cadastral(cliente):
+    response = HttpResponse(content_type="application/pdf")
+    response[
+        "Content-Disposition"
+    ] = f'filename="Ficha Cadastral {cliente}.pdf'
+    buffer = BytesIO()
+    pdf = canvas.Canvas(buffer)
+    header(pdf)
+    pdf.setTitle(f"FICHA CADASTRAL {cliente}.pdf")
+    pdf.save()
+    buffer.seek(0)
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+    return response
