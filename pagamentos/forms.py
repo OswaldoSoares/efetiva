9209f5e01ae9a4026a58231f0fbb94a5a1799937
OswaldoSoares <oswaldosoares@gmail.com>
2024-03-05 10:00:14 -0300

    Codigo com no máximo 79 colunas, ao salvar com black

--- pagamentos/forms.py
+++ pagamentos/forms.py
@@ -1,16 +1,22 @@
 from django import forms
 
-from pessoas.models import CartaoPonto
+from pessoas.models import CartaoPonto, Agenda
 
 
 class MeuTimeInput(forms.TimeInput):
-    input_type = 'time'
+    input_type = "time"
 
 
 class CadastraCartaoPonto(forms.ModelForm):
     class Meta:
         model = CartaoPonto
-        fields = '__all__'
-        labels = {'Entrada': 'HORA ENTRADA', 'Saida': 'HORA SAIDA'}
-        widgets = {'Entrada': MeuTimeInput(attrs={'class': 'form-control', 'pattern': '[0-9]{2}:[0-9]{2}'}),
-                   'Saida': MeuTimeInput(attrs={'class': 'form-control', 'pattern': '[0-9]{2}:[0-9]{2}'})}
+        fields = "__all__"
+        labels = {"Entrada": "HORA ENTRADA", "Saida": "HORA SAIDA"}
+        widgets = {
+            "Entrada": MeuTimeInput(
+                attrs={"class": "form-control", "pattern": "[0-9]{2}:[0-9]{2}"}
+            ),
+            "Saida": MeuTimeInput(
+                attrs={"class": "form-control", "pattern": "[0-9]{2}:[0-9]{2}"}
+            ),
+        }
