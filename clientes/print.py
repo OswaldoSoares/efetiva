9bc1b2d4df34f97ee398366d8556361dc64770c8
OswaldoSoares <oswaldosoares@gmail.com>
2024-06-13 15:15:03 -0300

    Import Label from reportlab.graphics.chart.textlabels

--- clientes/print.py
+++ clientes/print.py
@@ -10,6 +10,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from reportlab.graphics.shapes import Drawing
 from reportlab.graphics.charts.barcharts import VerticalBarChart
+from reportlab.graphics.charts.textlabels import Label
 from website.facade import cmp
 from website.print import header
 from transefetiva.settings.settings import STATIC_ROOT
