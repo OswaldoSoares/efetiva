9634fd237f12be45893292d8828c760df0360ace
OswaldoSoares <oswaldosoares@gmail.com>
2024-08-12 10:56:40 -0300

    Criada função calcula_diferenca com docstring

--- core/tools.py
+++ core/tools.py
@@ -15,3 +15,31 @@ def apos_meia_noite(hora):
     meia_noite = time(0, 0)
     is_apos_meia_noite = hora > meia_noite
     return is_apos_meia_noite
+
+
+def calcular_diferenca(data, inicial, final):
+    """
+    Calcula a diferença de tempo entre dois horários no mesmo dia e retorna
+    como datetime.time.
+
+    Args:
+        data (datetime.date): A data associada aos horários.
+        inicial (datetime.time): O horário inicial.
+        final (datetime.time): O horário final, que pode ser `None`
+                               ou `time(0, 0)` se não estiver definido.
+
+    Returns:
+        datetime.time: A diferença de tempo entre os dois horários.
+                       Retorna `time(0, 0)` se `final` for `None`
+                       ou `time(0, 0)` ou se `final` for antes de `inicial`.
+    """
+    periodo = timedelta(hours=0, minutes=0)
+    if final and final != time(0, 0):
+        inicial = datetime.combine(data, inicial)
+        final = datetime.combine(data, final)
+        if inicial < final:
+            periodo = final - inicial
+    total_segundos = int(periodo.total_seconds())
+    horas = total_segundos // 3600
+    minutos = (total_segundos % 3600) // 60
+    return time(horas, minutos)
