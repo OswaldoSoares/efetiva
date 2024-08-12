5cb3a4306eb5c4ed75c23e85f0e05a90b0943124
OswaldoSoares <oswaldosoares@gmail.com>
2024-08-12 10:56:10 -0300

    Criada função apos_meia_noite com docstring

--- core/tools.py
+++ core/tools.py
@@ -1,2 +1,17 @@
 """ MÓDULO COM FUNÇÕES QUE SERÃO USADAS EM TODO O PROJETO """
 from datetime import datetime, time, timedelta
+
+
+def apos_meia_noite(hora):
+    """
+    Verifica se a hora fornecida é após a meia-noite.
+
+    Args:
+        hora (datetime.time): A hora a ser verificada.
+
+    Returns:
+        bool: True se a hora for após a meia-noite, False caso contrário.
+    """
+    meia_noite = time(0, 0)
+    is_apos_meia_noite = hora > meia_noite
+    return is_apos_meia_noite
