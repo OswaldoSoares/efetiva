eb1248a30c04e38105273e7b8f53aa46a4a9d65e
OswaldoSoares <oswaldosoares@gmail.com>
2024-08-13 20:45:20 -0300

    Removida função convertemp

--- minutas/views.py
+++ minutas/views.py
@@ -49,16 +49,6 @@ from .forms import (
 from .models import Minuta, MinutaColaboradores, MinutaItens, MinutaNotas
 
 
-def convertemp(mm):
-    """
-    Converte milimetros em pontos - Criação de Relatórios
-
-    :param mm: milimetros
-    :return: pontos
-    """
-    return mm / 0.352777
-
-
 def excluiminutaitens(idminutaitens):
     """
     Função para excluir um item da Minuta
