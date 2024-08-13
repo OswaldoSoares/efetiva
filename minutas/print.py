a6f103bf214c118ecc8a3929326e6ad975e1d84e
OswaldoSoares <oswaldosoares@gmail.com>
2024-08-13 20:42:50 -0300

    Função imprime_minuta movida do módulo minutas.views

--- minutas/print.py
+++ minutas/print.py
@@ -223,3 +223,349 @@ def body(pdf, contexto, titulo):
     )
     pdf.roundRect(cmp(10), cmp(10), cmp(277), cmp(190), 10)
     return pdf
+
+
+# TODO Removida do views.py fazer refatoração
+def imprime_minuta(request, idmin):
+    minuta = Minuta.objects.get(idMinuta=idmin)
+    contato = FoneContatoCliente.objects.filter(idCliente=minuta.idCliente)
+    veiculo = ""
+    if minuta.idVeiculo_id:
+        veiculo = Veiculo.objects.get(idVeiculo=minuta.idVeiculo_id)
+    colaboradores = MinutaColaboradores.objects.filter(idMinuta=idmin)
+    motorista = ""
+    ajudante = ""
+    if colaboradores:
+        minutacolaboradores = MinutaColaboradores.objects.filter(
+            idMinuta=idmin, Cargo="MOTORISTA"
+        )
+        motorista = [item.idPessoal for item in minutacolaboradores]
+        motorista = motorista[0]
+        ajudante = MinutaColaboradores.objects.filter(
+            idMinuta=idmin, Cargo="AJUDANTE"
+        )
+    response = HttpResponse(content_type="application/pdf")
+    buffer = BytesIO()
+
+    # Create the PDF object, using the BytesIO object as its "file."
+    pdf = canvas.Canvas(buffer)
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+
+    url = f"{STATIC_ROOT}/website/img/transportadora.jpg"
+    pdf.roundRect(cmp(10), cmp(10), cmp(190), cmp(277), 10)
+    pdf.drawImage(url, cmp(12), cmp(265), cmp(40), cmp(20))
+    pdf.setFont("Times-Bold", 18)
+    pdf.drawString(
+        cmp(56),
+        cmp(279),
+        "TRANSEFETIVA TRANSPORTE - EIRELLI - ME",
+    )
+    pdf.setFont("Times-Roman", 12)
+    pdf.drawString(
+        cmp(53),
+        cmp(273),
+        "RUA OLIMPIO PORTUGAL, 245 - MOOCA - SÃO PAULO - SP - CEP 03112-010",
+    )
+    pdf.setFont("Times-Roman", 12)
+    pdf.drawString(
+        cmp(70),
+        cmp(268),
+        "(11) 2305-0582 - WHATSAPP (11) 94167-0583",
+    )
+    pdf.drawString(
+        cmp(67),
+        cmp(263),
+        "e-mail: transefetiva@terra.com.br - "
+        "operacional.efetiva@terra.com.br",
+    )
+    pdf.line(cmp(10), cmp(260), cmp(200), cmp(260))
+    # ----
+    pdf.setFillColor(HexColor("#FFFFFF"))
+    pdf.setStrokeColor(HexColor("#FFFFFF"))
+    pdf.rect(
+        cmp(10),
+        cmp(254.1),
+        cmp(190),
+        cmp(5.6),
+        fill=1,
+        stroke=1,
+    )
+    pdf.setStrokeColor(HexColor("#000000"))
+    pdf.setFillColor(HexColor("#000000"))
+    # ----
+    pdf.setFont("Times-Roman", 12)
+    pdf.drawString(
+        cmp(10),
+        cmp(255.8),
+        "ORDEM DE SERVIÇO Nº: " + str(minuta.Minuta),
+    )
+    pdf.drawRightString(
+        cmp(200),
+        cmp(255.8),
+        "DATA: " + minuta.DataMinuta.strftime("%d/%m/%Y"),
+    )
+    # ----
+    pdf.setFont("Times-Roman", 10)
+    pdf.setFillColor(HexColor("#c1c1c1"))
+    pdf.rect(cmp(10), cmp(249), cmp(95), cmp(5), fill=1)
+    pdf.setFillColor(HexColor("#000000"))
+    pdf.drawCentredString(cmp(57.5), cmp(250.3), "DADOS DO CLIENTE")
+    # ----
+    pdf.setFont("Times-Roman", 8)
+    pdf.drawString(
+        cmp(11),
+        cmp(246),
+        "CLIENTE: " + str(minuta.idCliente.Nome),
+    )
+    endereco = minuta.idCliente.Endereco + " - " + minuta.idCliente.Bairro
+    if len(endereco) > 45:
+        pdf.drawString(
+            cmp(11),
+            cmp(242),
+            "ENDEREÇO: " + endereco[0:45] + "...",
+        )
+    else:
+        pdf.drawString(
+            cmp(11),
+            cmp(242),
+            "ENDEREÇO: "
+            + minuta.idCliente.Endereco
+            + " - "
+            + minuta.idCliente.Bairro,
+        )
+    pdf.drawString(
+        cmp(27),
+        cmp(238),
+        minuta.idCliente.Cidade
+        + " - "
+        + minuta.idCliente.Estado
+        + " - "
+        + minuta.idCliente.CEP,
+    )
+    pdf.drawString(
+        cmp(11),
+        cmp(234),
+        "INSCRIÇÃO CNPJ:" + minuta.idCliente.CNPJ,
+    )
+    pdf.drawString(
+        cmp(11),
+        cmp(230),
+        "INSCRIÇÃO ESTADUAL: " + minuta.idCliente.IE,
+    )
+    if contato:
+        pdf.drawString(
+            cmp(11),
+            cmp(226),
+            "CONTATO: " + contato[0].Contato,
+        )
+        pdf.drawString(cmp(11), cmp(222), "TELEFONE: " + contato[0].Fone)
+    # ----
+    pdf.line(cmp(105), cmp(249), cmp(105), cmp(217))
+    # ----
+    pdf.setFont("Times-Roman", 10)
+    pdf.setFillColor(HexColor("#c1c1c1"))
+    pdf.rect(
+        cmp(105),
+        cmp(249),
+        cmp(95),
+        cmp(5),
+        fill=1,
+    )
+    pdf.setFillColor(HexColor("#000000"))
+    pdf.drawCentredString(
+        cmp(152.5), cmp(250.3), "DADOS DO SERVIÇO SOLICITADO"
+    )
+    # ----
+    pdf.setFont("Times-Roman", 8)
+    y = 250
+    if minuta.idCategoriaVeiculo:
+        y -= 4
+        pdf.drawString(
+            cmp(106),
+            cmp(y),
+            "VEÍCULO: {}".format(minuta.idCategoriaVeiculo),
+        )
+        if veiculo:
+            pdf.drawRightString(cmp(199), cmp(y), "PLACA: {}".format(veiculo))
+    if motorista:
+        y -= 4
+        pdf.drawString(
+            cmp(106),
+            cmp(y),
+            "MOTORISTA: {}".format(motorista),
+        )
+    if ajudante:
+        if ajudante.count() == 1:
+            y -= 4
+            pdf.drawString(
+                cmp(106),
+                cmp(y),
+                "AJUDANTE: {}".format(ajudante[0].idPessoal),
+            )
+        else:
+            for x in range(ajudante.count()):
+                y -= 4
+                if x == 0:
+                    pdf.drawString(
+                        cmp(106),
+                        cmp(y),
+                        str(ajudante.count())
+                        + " AJUDANTES: "
+                        + str(ajudante[x].idPessoal),
+                    )
+                else:
+                    pdf.drawString(
+                        cmp(126),
+                        cmp(y),
+                        str(ajudante[x].idPessoal),
+                    )
+    if minuta.KMInicial:
+        y -= 4
+        pdf.drawString(
+            cmp(106),
+            cmp(y),
+            "KM Inicial: " + str(minuta.KMInicial),
+        )
+    y -= 4
+    pdf.drawString(
+        cmp(106),
+        cmp(y),
+        "HORA INICIAL: " + minuta.HoraInicial.strftime("%H:%M"),
+    )
+    # ----
+    pdf.setFont("Times-Roman", 10)
+    pdf.setFillColor(HexColor("#c1c1c1"))
+    pdf.rect(
+        cmp(10),
+        cmp(212),
+        cmp(95),
+        cmp(5),
+        fill=1,
+    )
+    pdf.setFillColor(HexColor("#000000"))
+    pdf.drawCentredString(
+        cmp(57.5),
+        cmp(213.3),
+        "DESCRIÇÃO DO SERVIÇO EXECUTADO",
+    )
+    # ----
+    pdf.line(cmp(105), cmp(212), cmp(105), cmp(172))
+    # TODO Excluido custo operacional da minuta 18/09/2020
+    # pdf.setFont("Times-Roman", 10)
+    # pdf.setFillColor(HexColor("#c1c1c1"))
+    # pdf.rect(cmp(105), cmp(212), cmp(95), cmp(5), fill=1)
+    # pdf.setFillColor(HexColor("#000000"))
+    # pdf.drawCentredString(cmp(152.5), cmp(213.3), 'CUSTO OPERACIONAL')
+    # ----
+    pdf.setFont("Times-Roman", 10)
+    pdf.setFillColor(HexColor("#c1c1c1"))
+    pdf.rect(
+        cmp(10),
+        cmp(167),
+        cmp(190),
+        cmp(5),
+        fill=1,
+    )
+    pdf.setFillColor(HexColor("#000000"))
+    pdf.drawCentredString(cmp(105), cmp(168.3), "DESCRIÇÃO DOS SERVIÇOS")
+    # ----
+    pdf.setFont("Times-Roman", 10)
+    pdf.setFillColor(HexColor("#c1c1c1"))
+    pdf.rect(
+        cmp(10),
+        cmp(87),
+        cmp(190),
+        cmp(5),
+        fill=1,
+    )
+    pdf.setFillColor(HexColor("#000000"))
+    pdf.drawCentredString(cmp(105), cmp(88.3), "LOCAIS DE ENTREGAS E COLETAS")
+    pdf.setFont("Times-Roman", 8)
+    entregacoleta = ""
+    if minuta.Entrega and minuta.Coleta:
+        entregacoleta = (
+            "ENTREGA: " + minuta.Entrega + " - COLETA: " + minuta.Coleta
+        )
+    elif minuta.Entrega:
+        entregacoleta = "ENTREGA: " + minuta.Entrega
+    elif minuta.Coleta:
+        entregacoleta = "COLETA: " + minuta.Coleta
+    if len(entregacoleta) > 115:
+        wrap_entcol = wrap(entregacoleta, width=115)
+        y = 87.4
+        for linha in range(len(wrap_entcol)):
+            if linha == 4:
+                break
+            y -= 3
+            pdf.drawString(cmp(11), cmp(y), wrap_entcol[linha])
+    else:
+        pdf.drawString(cmp(11), cmp(84.4), entregacoleta)
+        # ----
+    pdf.setFont("Times-Roman", 10)
+    pdf.setFillColor(HexColor("#c1c1c1"))
+    pdf.rect(
+        cmp(10),
+        cmp(69),
+        cmp(190),
+        cmp(5),
+        fill=1,
+    )
+    pdf.setFillColor(HexColor("#000000"))
+    pdf.drawCentredString(cmp(105), cmp(70.3), "OBSERVAÇÕES")
+    pdf.setFont("Times-Roman", 8)
+    observ = minuta.Obs
+    if len(observ) > 115:
+        wrap_obs = wrap(observ, width=115)
+        y = 69.4
+        for linha in range(len(wrap_obs)):
+            if linha == 4:
+                break
+            y -= 3
+            pdf.drawString(cmp(11), cmp(y), wrap_obs[linha])
+    else:
+        pdf.drawString(cmp(11), cmp(66.4), observ)
+        # ----
+    pdf.setFont("Times-Roman", 10)
+    pdf.setFillColor(HexColor("#c1c1c1"))
+    pdf.rect(cmp(10), cmp(51), cmp(190), cmp(5), fill=1)
+    pdf.setFillColor(HexColor("#000000"))
+    pdf.drawCentredString(cmp(105), cmp(52.3), "KILOMETRAGEM")
+    pdf.setFont("Times-Roman", 12)
+    pdf.drawString(cmp(11), cmp(45.5), "KM INICIAL: ")
+    pdf.drawString(cmp(106), cmp(45.5), "KM FINAL: ")
+    # ----
+    pdf.line(cmp(10), cmp(43), cmp(200), cmp(43))
+    # ----
+    pdf.roundRect(cmp(12), cmp(12), cmp(101), cmp(19), 3)
+    pdf.setFont("Times-Roman", 7)
+    textominuta = (
+        "A TRANSEFETIVA TRANSPORTE - EIRELI - ME, só se responsabilizará pela mercadoria que o\ncliente"
+        " pagar seguro antes da mesma ser carregada. A responsabilidade da mercadoria e demais en-\ncargos"
+        " nela contida é unicamente do cliente. É de responsábilidade do cliente MULTAS DE TRAN-\nSITO e"
+        " outros encargos que podem ser cobrados, devido as restrições de horário e locais de entrega.\n"
+        "Reconheço estar de pleno acordo com o serviço executado e dos dados informados, não tendo"
+        " recla-\nmações posteriores à assinatura deste documento."
+    )
+    textobject = pdf.beginText(cmp(13), cmp(28))
+    for line in textominuta.splitlines(False):
+        textobject.textLine(line.rstrip())
+        pdf.drawText(textobject)
+    # ----
+    pdf.setFont("Times-Roman", 10)
+    pdf.line(cmp(118), cmp(15), cmp(194), cmp(15))
+    pdf.drawString(
+        cmp(118),
+        cmp(12),
+        "DATA, ASSINATURA E CARIMBO DO CLIENTE",
+    )
+    # Close the PDF object cleanly.
+    pdf.setTitle("Minuta.pdf")
+    pdf.showPage()
+    pdf.save()
+    # Get the value of the BytesIO buffer and write it to the response.
+    buffer.seek(0)
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+    return response
