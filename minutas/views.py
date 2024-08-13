ab85f6a490f08b5d428f2c524400d3e5bbca4a53
OswaldoSoares <oswaldosoares@gmail.com>
2024-08-13 19:34:45 -0300

    Deletada a função edita_minuta_km_inicial, versão antiga

--- minutas/views.py
+++ minutas/views.py
@@ -886,13 +886,6 @@ def remove_minuta_colaborador(request):
     return data
 
 
-def edita_minuta_km_inicial(request):
-    c_idminuta = request.POST.get("idMinuta")
-    c_kminicial = request.POST.get("KMInicial")
-    data = edita_km_inicial(request, c_idminuta, c_kminicial)
-    return data
-
-
 def edita_minuta_km_final(request):
     c_idminuta = request.POST.get("idMinuta")
     c_kmfinal = request.POST.get("KMFinal")
