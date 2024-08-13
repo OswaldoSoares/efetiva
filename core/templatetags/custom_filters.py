7330285afff11b2805111b749b332ac32a12782a
OswaldoSoares <oswaldosoares@gmail.com>
2024-08-13 10:31:44 -0300

    Criada a função default_if_zero com Docstring

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -8,3 +8,22 @@ personalizada.
 from django import template
 
 register = template.Library()
+
+
+@register.filter(name="default_if_zero")
+def default_if_zero(value, arg):
+    """
+    Retorna um valor padrão se o valor fornecido for zero.
+
+    Este filtro é utilizado em templates para exibir um valor alternativo
+    quando o valor original é zero.
+
+    Args:
+        value (int or float): O valor a ser verificado.
+        arg (any): O valor a ser retornado se o valor for zero.
+
+    Returns:
+        any: O valor original se não for zero, caso contrário, o valor
+        padrão fornecido.
+    """
+    return arg if value == 0 else value
