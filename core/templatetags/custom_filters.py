043d26696af97085c4d08eccd0e1d9c683d2eb98
OswaldoSoares <oswaldosoares@gmail.com>
2024-08-13 10:27:53 -0300

    Adicionado Docstring para o módulo

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -1,1 +1,7 @@
+"""
+Este módulo contém filtros personalizados para uso com o sistema de
+templates do Django.
 
+Os filtros ajudam a manipular e exibir dados em templates de forma
+personalizada.
+"""
