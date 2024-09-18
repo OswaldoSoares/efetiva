29ce3f1ebf2dbd429a184d87b0d85d1cf0e38e1e
OswaldoSoares <oswaldosoares@gmail.com>
2024-09-18 15:56:30 -0300

    Criada função executarAjax

--- website/static/website/js/minutas.js
+++ website/static/website/js/minutas.js
@@ -1048,3 +1048,22 @@ function atualizaAposComum(data) {
     verificaCheckboxClasse("total-recebe")
     calcularTotais("recebe")
 }
+
+function executarAjax(url, type, data, sucessoCallback) {
+    $.ajax({
+        type: type,
+        url: url,
+        data: data,
+        beforeSend: function() {
+            $(".box-loader").show();
+        },
+        success: function(response) {
+            sucessoCallback(response);
+            $(".box-loader").hide();
+        },
+        error: function(error) {
+            console.log(error);
+            $(".box-loader").hide();
+        }
+    });
+}
