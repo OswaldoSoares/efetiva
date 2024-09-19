6d7ef964c7a4581cf2984da2e65ea7ba8125e144
OswaldoSoares <oswaldosoares@gmail.com>
2024-09-19 14:58:54 -0300

    Criada constant TIPOS_CALCULO

---
+++ core/constants.py
@@ -0,0 +1,10 @@
+TIPOS_CALCULO = [
+    "porcentagem_nota",
+    "hora",
+    "quilometragem",
+    "entregas",
+    "saida",
+    "capacidade_peso",
+    "entregas_quilos",
+    "entregas_volume",
+]
