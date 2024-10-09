4403f38e26ed38628d73b0288afe309348313713
OswaldoSoares <oswaldosoares@gmail.com>
2024-10-09 14:33:19 -0300

    Adicionado itens do card_categoria_colaborador

---
+++ pessoas/itens_card.py
@@ -0,0 +1,17 @@
+categorias_colaborador = [
+    {
+        "categoria": "MENSALISTAS ATIVOS",
+        "class": "icofont-checked disabled",
+        "tipo": "MENSALISTA",
+    },
+    {
+        "categoria": "AVULSOS ATIVOS",
+        "class": "icofont-square",
+        "tipo": "DIVERSOS",
+    },
+    {
+        "categoria": "COLABORADORES INATIVOS",
+        "class": "icofont-square",
+        "tipo": "",
+    },
+]
