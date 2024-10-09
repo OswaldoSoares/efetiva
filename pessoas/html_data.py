d80e1bd9530e4c08c705a47c15bc0a83dbd5d70a
OswaldoSoares <oswaldosoares@gmail.com>
2024-10-09 14:34:53 -0300

    Criada a função html_card_lista_colaboradores

--- pessoas/html_data.py
+++ pessoas/html_data.py
@@ -1,1 +1,10 @@
 from django.template.loader import render_to_string
+
+
+def html_card_listia_colaboradores(request, data, contexto):
+    data["html-card-colaboradores"] = render_to_string(
+        "pessoas/card_colaboradores.html",
+        contexto,
+        request=request,
+    )
+    return data
