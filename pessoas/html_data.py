d43df3e9aab7b8426e84f62571cc14ee60a480be
OswaldoSoares <oswaldosoares@gmail.com>
2024-10-15 13:44:00 -0300

    Alterada ordem dos parametros da função html_card_lista_colaboradores

--- pessoas/html_data.py
+++ pessoas/html_data.py
@@ -1,7 +1,7 @@
 from django.template.loader import render_to_string
 
 
-def html_card_lista_colaboradores(request, data, contexto):
+def html_card_lista_colaboradores(request, contexto, data):
     data["html-card-colaboradores"] = render_to_string(
         "pessoas/card_colaboradores.html",
         contexto,
