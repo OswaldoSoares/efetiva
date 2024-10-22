5f56c040ee9773b71301bdff27ae64e8d44bf467
OswaldoSoares <oswaldosoares@gmail.com>
2024-10-22 13:46:54 -0300

    Codigo com no máximo 79 colunas, ao salvar com black

--- pessoas/html_data.py
+++ pessoas/html_data.py
@@ -3,9 +3,7 @@ from django.template.loader import render_to_string
 
 def html_card_lista_colaboradores(request, contexto, data):
     data["html-card-colaboradores"] = render_to_string(
-        "pessoas/card_colaboradores.html",
-        contexto,
-        request=request,
+        "pessoas/card_colaboradores.html", contexto, request=request
     )
     return data
 
