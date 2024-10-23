5f325af8d1241aba986fdfdef810c49cff2d2f43
OswaldoSoares <oswaldosoares@gmail.com>
2024-10-22 21:30:30 -0300

    Removida função create_data_form_adiciona_documento_colaborador

--- pessoas/facade.py
+++ pessoas/facade.py
@@ -853,12 +853,6 @@ def rescisao_descimo_terceiro_meses(data_inicial, data_final):
     return meses
 
 
-def create_data_form_adiciona_documento_colaborador(request, contexto):
-    data = dict()
-    html_form_adiciona_documento_colaborador(request, contexto, data)
-    return JsonResponse(data)
-
-
 def create_data_form_exclui_documento_colaborador(request, contexto):
     data = dict()
     html_form_confirma_exclusao(request, contexto, data)
