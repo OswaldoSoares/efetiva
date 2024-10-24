3123e175b5e7991c0d806dd92cef443dbdd943c1
OswaldoSoares <oswaldosoares@gmail.com>
2024-10-23 22:40:51 -0300

    Removida função js-altera-telefone-colaborador

--- website/static/website/js/pessoas.js
+++ website/static/website/js/pessoas.js
@@ -226,30 +226,6 @@ $(document).on('submit', '.js-gera-periodo-ferias', function(event) {
     });
 });
 
-$(document).on('click', '.js-altera-telefone-colaborador', function() {
-    var idfonepessoal = $(this).data('idfonepessoal')
-    var idpessoal = $(this).data('idpessoal')
-    $.ajax({
-        type: "GET",
-        url: "/pessoas/altera_telefone_colaborador",
-        data: {
-            idfonepessoal: idfonepessoal,
-            idpessoal: idpessoal,
-        },
-        beforeSend: function() {
-            $(".box-loader").show()
-        },
-        success: function(data) {
-            $(".card-form-colaborador").html(data.html_form_fone_colaborador)
-            $(".card-form-colaborador").show()
-            $(".box-loader").hide()
-        },
-        error: function(errorThrown) {
-            console.log("error: " + errorThrown)
-        }
-    });
-});
-
 $(document).on('click', '.js-verbas-rescisoria', function() {
     var idpessoal = $(this).data('idpessoal')
     $.ajax({
