4eed5160e1f46a54c9474818eb40c00f493d4bd4
OswaldoSoares <oswaldosoares@gmail.com>
2024-10-29 23:20:53 -0300

    Criada a função html_card_vales_colaborador

--- pessoas/html_data.py
+++ pessoas/html_data.py
@@ -36,6 +36,13 @@ def html_card_contas_colaborador(request, contexto, data):
     return data
 
 
+def html_card_vales_colaborador(request, contexto, data):
+    data["html-card-vales-colaborador"] = render_to_string(
+        "pessoas/card_vales_colaborador.html", contexto, request=request
+    )
+    return data
+
+
 def html_modal_colaborador(request, contexto):
     modal_html = render_to_string(
         "pessoas/modal_colaborador.html", contexto, request=request
