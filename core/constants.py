9e87c5f68e8348a39f2c8ed0ad308037ace1b0f5
OswaldoSoares <oswaldosoares@gmail.com>
2024-11-01 14:53:10 -0300

    Criada constant MESES

--- core/constants.py
+++ core/constants.py
@@ -17,6 +17,21 @@ Exemplo de uso:
     from .constants import TIPOS_FORMAS_CALCULO
 """
 
+MESES = {
+    1: "JANEIRO",
+    2: "FEVEREIRO",
+    3: "MARÇO",
+    4: "ABRIL",
+    5: "MAIO",
+    6: "JUNHO",
+    7: "JULHO",
+    8: "AGOSTO",
+    9: "SETEMBRO",
+    10: "OUTUBRO",
+    11: "NOVEMBRO",
+    12: "DEZEMBRO",
+}
+
 # Constants Módulo Pessoal
 CATEGORIAS = [
     "AGREGADO",
