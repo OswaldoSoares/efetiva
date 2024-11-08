19b96c731c9754b909331828828fa44d19f330fe
OswaldoSoares <oswaldosoares@gmail.com>
2024-11-08 12:36:48 -0300

    Ordenar contra_cheque_itens pelo campo Registro

--- pessoas/facade.py
+++ pessoas/facade.py
@@ -782,7 +782,7 @@ def create_contexto_contra_cheque(request):
     ).first()
     contra_cheque_itens = ContraChequeItens.objects.filter(
         idContraCheque_id=request.GET.get("id_contra_cheque")
-    )
+    ).order_by("Registro")
     print(contas)
     if contas:
         update_contas_bancaria_obs(contra_cheque, contas, "contas")
