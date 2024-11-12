61568660c02aa09527bd11ce0057506b007f695f
OswaldoSoares <oswaldosoares@gmail.com>
2024-11-12 15:24:00 -0300

    Criada a função html_card_colaboradores

--- pagamentos/html_data.py
+++ pagamentos/html_data.py
@@ -1,1 +1,8 @@
 from django.template.loader import render_to_string
+
+
+def html_card_colaboradores(request, contexto, data):
+    data["html-card-colaboradores"] = render_to_string(
+        "pagamentos/card_colaboradores.html", contexto, request=request
+    )
+    return data
