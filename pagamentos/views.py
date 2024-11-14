636dbd4fa8478496d35f21eff337ba412eac27e2
OswaldoSoares <oswaldosoares@gmail.com>
2024-11-14 10:35:01 -0300

    Alterado parametro na chamada da função create_contexto_colaborador e excluída variáveis idpessoal e mes_ano

--- pagamentos/views.py
+++ pagamentos/views.py
@@ -29,9 +29,7 @@ def selecionar_mes_pagamento(request):
 
 
 def selecionar_colaborador(request):
-    idpessoal = request.GET.get("idpessoal")
-    mes_ano = request.GET.get("mes_ano")
-    contexto = facade.create_contexto_colaborador(idpessoal, mes_ano)
+    contexto = facade.create_contexto_colaborador(request)
     data = facade.create_data_seleciona_funcionario(request, contexto)
     return data
 
