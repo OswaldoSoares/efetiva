943b67fd88511499eaee3d2b6052c3070f2dd9ff
OswaldoSoares <oswaldosoares@gmail.com>
2024-11-29 08:04:59 -0300

    Criada a função save_data_demissao_colaborador

--- pessoas/facade.py
+++ pessoas/facade.py
@@ -698,6 +698,27 @@ def excluir_cartao_ponto_mes_seguinte_rescisao(id_pessoal, demissao):
     ).delete()
 
 
+def save_data_demissao_colaborador(request):
+    id_pessoal = request.POST.get("id_pessoal")
+    demissao_str = request.POST.get("demissao")
+
+    if not id_pessoal or not demissao_str:
+        return {"mensagem": "Parâmetros inválidos"}
+
+    try:
+        demissao = datetime.strptime(demissao_str, "%Y-%m-%d")
+    except ValueError:
+        return {"mensagem": "Formato de data inválido"}
+
+    registrar_contra_cheque(id_pessoal, demissao, "RESCISÃO")
+    atualizar_cartao_ponto_rescisao(id_pessoal, demissao)
+    excluir_cartao_ponto_mes_seguinte_rescisao(id_pessoal, demissao)
+
+    Pessoal.objects.filter(idPessoal=id_pessoal).update(DataDemissao=demissao)
+
+    return {"mensagem": "Data de demissão inserida com sucesso"}
+
+
 def get_decimo_terceiro_colaborador(id_pessoal):
     decimo_terceiro = DecimoTerceiro.objects.filter(
         idPessoal=id_pessoal
