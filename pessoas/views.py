2f8820696777d84f23bff7c79df4f4f7e7f19128
OswaldoSoares <oswaldosoares@gmail.com>
2025-01-07 16:30:43 -0300

    Criada a função alterar_conducao_colaborador

--- pessoas/views.py
+++ pessoas/views.py
@@ -187,6 +187,11 @@ def abonar_falta_colaborador(request):
     return facade.cartao_ponto_html_data(request, contexto)
 
 
+def alterar_conducao_colaborador(request):
+    contexto = facade.alterar_cartao_ponto_conducao(request)
+    return facade.cartao_ponto_html_data(request, contexto)
+
+
 def bloqueia_pessoa(request, idpessoa):
     facade.altera_status(idpessoa)
     return redirect("indexpessoal")
