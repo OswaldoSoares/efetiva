26dd560a12fff6739f3bd51a8e5cdbaa8a5b1cd8
OswaldoSoares <oswaldosoares@gmail.com>
2025-02-12 13:36:24 -0300

    Adicionado html_data.html_card_vale_transporte_colaborador na função colaborador_html_data

--- pessoas/facade.py
+++ pessoas/facade.py
@@ -2551,6 +2551,7 @@ def colaborador_html_data(request, contexto):
         html_data.html_card_fones_colaborador,
         html_data.html_card_contas_colaborador,
         html_data.html_card_salario_colaborador,
+        html_data.html_card_vale_transporte_colaborador,
     ]
     html_ferias_colaborador(request, contexto, data)
     return gerar_data_html(html_functions, request, contexto, data)
