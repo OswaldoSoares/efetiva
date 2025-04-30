102f003e0ae5b88291e37e014601b0a44dcbc1d2
OswaldoSoares <oswaldosoares@gmail.com>
2025-04-30 17:19:30 -0300

    Import xframe_options_exempt from django.views.decorators.clickjacking

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ import mimetypes
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import FileResponse, Http404
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 @login_required(login_url="login")
 def index_core(request):
