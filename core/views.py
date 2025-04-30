0c5611913f41d2ddb92fb0f8201f6c8b218e944a
OswaldoSoares <oswaldosoares@gmail.com>
2025-04-30 17:20:46 -0300

    Declarada a função def visualizar_arquivo

--- core/views.py
+++ core/views.py
@@ -10,3 +10,16 @@ from website.models import FileUpload
 @login_required(login_url="login")
 def index_core(request):
     return render(request, "core/index.html")
+
+
+@xframe_options_exempt
+def visualizar_arquivo(request, id_file_upload):
+    try:
+        arquivo = FileUpload.objects.get(idFileUpload=id_file_upload)
+        file_path = arquivo.uploadFile.path
+        content_type, _ = mimetypes.guess_type(file_path)
+
+        return FileResponse(open(file_path, "rb"), content_type=content_type)
+
+    except Exception as error:
+        raise Http404(f"Erro ao carregar arquivo: {error}")
