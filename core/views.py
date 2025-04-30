4407ca2ca1c407c0414823ed287dadfc800a3541
OswaldoSoares <oswaldosoares@gmail.com>
2025-04-30 17:20:07 -0300

    Import FileUpload from website.models

--- core/views.py
+++ core/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import FileResponse, Http404
 from django.views.decorators.clickjacking import xframe_options_exempt
+from website.models import FileUpload
+
 
 @login_required(login_url="login")
 def index_core(request):
