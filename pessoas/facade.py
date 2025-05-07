d35d8d6bcf86f6f2079ee6d7a03aad13b5bb472a
OswaldoSoares <oswaldosoares@gmail.com>
2025-05-07 12:32:27 -0300

    Declarada a função validar_modal_data_readmissao_colaborador

--- pessoas/facade.py
+++ pessoas/facade.py
@@ -1087,6 +1087,41 @@ def modal_data_readmissao_colaborador(id_pessoal, request):
     return JsonResponse({"modal_html": modal_html})
 
 
+def validar_modal_data_readmissao_colaborador(request: Any) -> JsonResponse:
+    if request.method != "POST":
+        return False
+
+    id_pessoal = request.POST.get("id_pessoal")
+    readmissao_str = request.POST.get("readmissao")
+
+    data_readmissao = datetime.strptime(readmissao_str, "%Y-%m-%d").date()
+    hoje = datetime.today().date()
+
+    colaborador = classes.Colaborador(id_pessoal)
+    data_demissao = colaborador.dados_profissionais.data_demissao
+    _, ultimo_dia_mes_demissao = primeiro_e_ultimo_dia_do_mes(
+        data_demissao.month, data_demissao.year
+    )
+
+    if data_readmissao > hoje:
+        return JsonResponse(
+            {
+                "error": "A data de readmissão não pode ser posterior ao dia de hoje."
+            },
+            status=400,
+        )
+
+    if data_readmissao <= ultimo_dia_mes_demissao.date():
+        return JsonResponse(
+            {
+                "error": "A data de readmissão deve ser posterior ao mês de demissão."
+            },
+            status=400,
+        )
+
+    return False
+
+
 def registrar_contra_cheque(id_pessoal, data_base, descricao):
     mes_por_extenso = obter_mes_por_numero(data_base.month)
     ano = data_base.year
