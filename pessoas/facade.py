ae92ba03ecb98a4aebb5de5b3041f90da5d727c0
OswaldoSoares <oswaldosoares@gmail.com>
2025-06-06 12:28:29 -0300

    Import Path from pathlib

--- pessoas/facade.py
+++ pessoas/facade.py
@@ -23,6 +23,7 @@ from django.template.loader import render_to_string
 from decimal import Decimal, ROUND_HALF_UP
 from PIL import Image, ImageDraw
 from typing import List, Dict, Any, Optional
+from pathlib import Path
 from despesas import facade as facade_multa
 
 from pagamentos import facade as facade_pagamentos
