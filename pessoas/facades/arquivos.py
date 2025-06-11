5a526951a5cf6efb111a0f3d011410efda4073c3
OswaldoSoares <oswaldosoares@gmail.com>
2025-06-10 22:39:44 -0300

    Removido import não usado

--- pessoas/facades/arquivos.py
+++ pessoas/facades/arquivos.py
@@ -1,6 +1,5 @@
 """ Responsável pelo logica do upload de documentos """
 import re
-from collections import Counter
 from typing import List, Dict
 from core import constants
 from website.models import FileUpload
