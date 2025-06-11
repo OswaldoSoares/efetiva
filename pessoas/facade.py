c51cc0c7e37131364b51e599c67de700d340d353
OswaldoSoares <oswaldosoares@gmail.com>
2025-06-11 08:43:12 -0300

    Realocada a função modal_data_demissao_colaborador do módulo facade.py -> demissao.py

--- pessoas/facade.py
+++ pessoas/facade.py
@@ -1038,24 +1038,6 @@ def delete_vale_colaborador(request):
     return {"mensagem": "Não foi possível excluir vale do colaborador"}
 
 
-def modal_data_demissao_colaborador(id_pessoal, request):
-    id_pessoal = (
-        request.POST.get("id_pessoal")
-        if request.method == "POST"
-        else request.GET.get("id_pessoal")
-    )
-    colaborador = classes.Colaborador(id_pessoal) if id_pessoal else False
-    hoje = datetime.today().date()
-    contexto = {
-        "colaborador": colaborador,
-        "hoje": hoje.strftime("%Y-%m-%d"),
-    }
-    modal_html = html_data.html_modal_data_demissao_colaborador(
-        request, contexto
-    )
-    return JsonResponse({"modal_html": modal_html})
-
-
 def validar_modal_data_demissao_colaborador(request: Any) -> JsonResponse:
     if request.method != "POST":
         return False
--- pessoas/facades/rescisao.py
+++ pessoas/facades/rescisao.py
@@ -3,3 +3,21 @@ from datetime import datetime
 from django.http import JsonResponse
 from pessoas import classes
 from pessoas import html_data
+
+
+def modal_data_demissao_colaborador(id_pessoal, request):
+    id_pessoal = (
+        request.POST.get("id_pessoal")
+        if request.method == "POST"
+        else request.GET.get("id_pessoal")
+    )
+    colaborador = classes.Colaborador(id_pessoal) if id_pessoal else False
+    hoje = datetime.today().date()
+    contexto = {
+        "colaborador": colaborador,
+        "hoje": hoje.strftime("%Y-%m-%d"),
+    }
+    modal_html = html_data.html_modal_data_demissao_colaborador(
+        request, contexto
+    )
+    return JsonResponse({"modal_html": modal_html})
