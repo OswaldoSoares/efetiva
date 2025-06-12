925b6697f5b0865e072af60b246fe2f9cd6b5caf
OswaldoSoares <oswaldosoares@gmail.com>
2025-06-12 13:38:47 -0300

    Alterada módulo da chamada da função mostrar_eventos_rescisorios_colaborador

--- pessoas/views.py
+++ pessoas/views.py
@@ -204,7 +204,9 @@ def adicionar_data_demissao_colaborador(request):
 
 
 def mostrar_eventos_rescisorios_colaborador(request):
-    contexto = facade.create_contexto_eventos_rescisorios_colaborador(request)
+    contexto = rescisao.create_contexto_eventos_rescisorios_colaborador(
+        request
+    )
     return facade.data_eventos_html_data(request, contexto)
 
 
