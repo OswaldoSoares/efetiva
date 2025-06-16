979fb6945682e3e5a981125e7a163d1eb7f61697
OswaldoSoares <oswaldosoares@gmail.com>
2025-06-16 13:25:29 -0300

    Declarada a função create_contexto_ferias_colaborador

--- pessoas/facades/ferias.py
+++ pessoas/facades/ferias.py
@@ -3,7 +3,7 @@ from typing import Dict
 from pessoas.models import Aquisitivo, Ferias
 
 
-def create_contexto_card_ferias(id_pessoal: int) -> Dict:
+def create_contexto_ferias_colaborador(id_pessoal: int) -> Dict:
     """
         Retorna um dicionário com os períodos aquititivos e os períodos de
         gozo das férias de um colaborador
