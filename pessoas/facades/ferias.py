375f5255c2c8be6ab3e5e4c8b6c3b9bdaaad95c1
OswaldoSoares <oswaldosoares@gmail.com>
2025-06-16 13:24:50 -0300

    Declarada a função create_contexto_card_ferias

--- pessoas/facades/ferias.py
+++ pessoas/facades/ferias.py
@@ -1,3 +1,20 @@
 """ Responsável pelas férias do colaborador """
 from typing import Dict
 from pessoas.models import Aquisitivo, Ferias
+
+
+def create_contexto_card_ferias(id_pessoal: int) -> Dict:
+    """
+        Retorna um dicionário com os períodos aquititivos e os períodos de
+        gozo das férias de um colaborador
+    Args:
+        id_pessoal:
+
+    Returns:
+        Dict: {"aquisitivos": aquisitivos, "gozo_ferias": gozo_ferias}
+
+    """
+    aquisitivos = Aquisitivo.objects.filter(idPessoal=id_pessoal)
+    gozo_ferias = Ferias.objects.filter(idPessoal=id_pessoal)
+
+    return {"aquisitivos": aquisitivos, "gozo_ferias": gozo_ferias}
