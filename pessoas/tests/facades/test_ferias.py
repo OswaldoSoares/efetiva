1cd662945fc07ca13795a1aa0797942110e899f0
OswaldoSoares <oswaldosoares@gmail.com>
2025-06-26 21:03:38 -0300

    Import calcular_dias_ferias_proporcionais, meses_proporcionais_ferias, calcula_valores_ferias from pessoas.facades.ferias

--- pessoas/tests/facades/test_ferias.py
+++ pessoas/tests/facades/test_ferias.py
@@ -2,3 +2,9 @@
 import pytest
 from decimal import Decimal
 from datetime import date
+
+from pessoas.facades.ferias import (
+    calcular_dias_ferias_proporcionais,
+    meses_proporcionais_ferias,
+    calcula_valores_ferias,
+)
