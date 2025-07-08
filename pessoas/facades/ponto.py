b1a07e88f6fc9241345c9400de1188f4e22340c8
Oswaldo Soares <oswaldosoares@gmail.com>
2025-07-08 14:41:04 -0300

    Declarada a função verificar_credencial_por_cpf

--- pessoas/facades/ponto.py
+++ pessoas/facades/ponto.py
@@ -16,3 +16,26 @@ from pessoas.models import DocPessoal, FidoCredential, RegistroPonto, Pessoal
 from core.tools import nome_curto
 
 
+def verificar_credencial_por_cpf(request, cpf):
+    cpf_formatado = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+    try:
+        documento = DocPessoal.objects.get(
+            TipoDocumento="CPF", Documento=cpf_formatado
+        )
+    except DocPessoal.DoesNotExist:
+        return JsonResponse({"cpf_encontrado": False})
+
+    cred = FidoCredential.objects.filter(idPessoal_id=documento.idPessoal_id)
+    tem_credencial = cred.exists()
+    liberado_pelo_rh = cred.first().liberado_pelo_rh if cred.exists() else False
+
+    return JsonResponse({
+        "cpf_encontrado": True,
+        "tem_credencial": tem_credencial,
+        "liberado_pelo_rh": liberado_pelo_rh,
+        "id_pessoal": documento.idPessoal_id,
+        "nome": documento.idPessoal.Nome,
+        "nome_curto": nome_curto(documento.idPessoal.Nome),
+        "cpf": cpf_formatado,
+    })
+
