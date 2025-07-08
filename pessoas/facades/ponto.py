62a1d84c03aea35a21678125d11fea8393c1e52d
Oswaldo Soares <oswaldosoares@gmail.com>
2025-07-08 11:31:06 -0300

    Import csrf_exempt from django.views.decorators.csrf

--- pessoas/facades/ponto.py
+++ pessoas/facades/ponto.py
@@ -6,5 +6,6 @@ import json
 from django.http import JsonResponse
 from django.utils.timezone import get_current_timezone, make_aware
 from django.utils.timezone import localdate, localtime
+from django.views.decorators.csrf import csrf_exempt
 
 
