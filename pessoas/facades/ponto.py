d4a4b5e5d269037a7b352e881b25cc12fe633c8f
Oswaldo Soares <oswaldosoares@gmail.com>
2025-07-08 11:35:28 -0300

    Import ec from cryptography.hazmat.primitives.asymmetric

--- pessoas/facades/ponto.py
+++ pessoas/facades/ponto.py
@@ -7,5 +7,6 @@ from django.http import JsonResponse
 from django.utils.timezone import get_current_timezone, make_aware
 from django.utils.timezone import localdate, localtime
 from django.views.decorators.csrf import csrf_exempt
+from cryptography.hazmat.primitives.asymmetric import ec
 
 
