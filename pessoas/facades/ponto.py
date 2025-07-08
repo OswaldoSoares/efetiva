99180df2c45e13cc567106e948a278d74e0c37e2
Oswaldo Soares <oswaldosoares@gmail.com>
2025-07-08 11:29:54 -0300

    Import get_current_timezone, make_awere, localdate, localtime from django.utils.timezone

--- pessoas/facades/ponto.py
+++ pessoas/facades/ponto.py
@@ -4,5 +4,7 @@ Módulo responsável pelo registro de ponto e contraoe do cartão de ponto.
 import base64
 import json
 from django.http import JsonResponse
+from django.utils.timezone import get_current_timezone, make_aware
+from django.utils.timezone import localdate, localtime
 
 
