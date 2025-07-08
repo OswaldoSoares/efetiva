639b1c20d2ec73891c66a729138cfe4b12a58306
Oswaldo Soares <oswaldosoares@gmail.com>
2025-07-08 10:25:23 -0300

    Configurado o corsheaders

--- transefetiva/settings/settings.py
+++ transefetiva/settings/settings.py
@@ -8,6 +8,7 @@ BASE_DIR = os.path.dirname(
 )
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -34,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -90,3 +92,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, config("MEDIA_ROOT_CONFIG", ""))
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
+
+CORS_ALLOW_ALL_ORIGINS = True
