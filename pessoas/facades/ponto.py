66c090258b2688e9cbdfe22e31181941089f5e35
Oswaldo Soares <oswaldosoares@gmail.com>
2025-08-01 09:44:54 -0300

    feat: adiciona funcionalidade de atualizar cartão de ponto pelo registro de ponto

--- pessoas/facades/ponto.py
+++ pessoas/facades/ponto.py
@@ -2,14 +2,15 @@
 Módulo responsável pelo registro de ponto e contrale do cartão de ponto.
 """
 import json
-from datetime import datetime, time
+from datetime import datetime, date, time
 from django.contrib.auth.hashers import check_password
 from django.http import JsonResponse
 from django.utils.timezone import get_current_timezone, localdate, localtime, make_aware
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET
-from pessoas.models import DocPessoal, RegistroPonto, SenhaAppPonto
-from core.tools import nome_curto
+from pessoas import classes
+from pessoas.models import CartaoPonto, DocPessoal, Ferias, RegistroPonto, SenhaAppPonto
+from core.tools import nome_curto, obter_feriados_sabados_domingos_mes
 
 
 @require_GET
@@ -135,3 +136,150 @@ def verificar_registro_colaborador_hoje(id_pessoal):
     saida = registros.filter(tipo="SAIDA").order_by("horario").last()
 
     return entrada, saida
+
+
+def obter_datas_nao_uteis_mes(primeiro_dia):
+    mes = primeiro_dia.month
+    ano = primeiro_dia.year
+    feriados, domingos, sabados = obter_feriados_sabados_domingos_mes(mes, ano)
+
+    datas_sabados = [
+        datetime.strptime(data, "%d/%m/%Y").date() for data in sabados
+    ]
+    datas_domingos = [
+        datetime.strptime(data, "%d/%m/%Y").date() for data in domingos
+    ]
+    datas_feriados = [
+        datetime.strptime(data.split(" - ")[0], "%d/%m/%Y").date()
+        for data in feriados
+    ]
+
+    return datas_sabados, datas_domingos, datas_feriados
+
+
+def dia_em_ferias(dia, id_pessoal):
+    ferias = Ferias.objects.filter(idPessoal_id=id_pessoal)
+
+    for periodo in ferias:
+        if periodo.DataInicial <= dia <= periodo.DataFinal:
+            return True
+    return False
+
+
+def aplicar_ausencia_dias_nao_util(cartao, dia, tipos_ausencia):
+    for tipo, lista in tipos_ausencia.items():
+        if dia in lista:
+            cartao.Ausencia = tipo
+            cartao.Conducao = False
+            cartao.Alteracao = "APP-PONTO"
+            cartao.save()
+            return True
+    return False
+
+
+def filter_by_local_date(queryset, field_name: str, dia: date):
+    """
+    Filtra um queryset por um DateTimeField, considerando o dia no
+    fuso horário local (TIME_ZONE).
+
+    :param queryset: QuerySet original.
+    :param field_name: Nome do campo DateTimeField (como string).
+    :param dia: Instância de datetime.date representando o dia local.
+    :return: QuerySet filtrado com registros daquele dia no horário local.
+    """
+    tz = get_current_timezone()
+
+    inicio = make_aware(datetime.combine(dia, datetime.min.time()), timezone=tz)
+    fim = make_aware(datetime.combine(dia, datetime.max.time()), timezone=tz)
+
+    filtro = {
+        f"{field_name}__range": (inicio, fim)
+    }
+
+    return queryset.filter(**filtro)
+
+
+def obter_horarios(registros_dia):
+    entrada = next(
+        (
+            localtime(r.horario).time() for r in registros_dia
+            if r.tipo == "ENTRADA"
+        ), None
+    )
+    saida = next(
+        (
+            localtime(r.horario).time() for r in reversed(registros_dia)
+            if r.tipo == "SAIDA"
+        ), None
+    )
+
+    return entrada, saida
+
+
+def processar_dia_util(cartao, dia, nao_uteis, data_implementacao, hoje, vale_transporte):
+    if dia < data_implementacao or dia >= hoje or dia in nao_uteis:
+        print(f"{dia} - Retornei")
+        return
+
+    print(f"processei {dia}")
+    registros_dia = filter_by_local_date(RegistroPonto.objects.filter(
+        idPessoal=cartao.idPessoal
+    ).order_by("horario"), "horario", dia)
+
+    entrada, saida = obter_horarios(registros_dia)
+
+    cartao.Entrada = entrada if entrada else cartao.Entrada
+    cartao.Saida = saida if saida else cartao.Saida
+
+    falta = not entrada and not saida
+    cartao.Ausencia = "FALTA" if falta else ""
+    cartao.Remunerado = not falta
+    cartao.Conducao = vale_transporte and not falta
+    cartao.Alteracao = "APP-PONTO"
+    cartao.save()
+
+
+def atualizar_cartao_ponto_pelo_registro_ponto(cartao_ponto):
+    primeiro_dia = cartao_ponto.first().Dia
+    hoje = datetime.today().date()
+    data_implementacao = datetime(2025, 7, 21).date()
+
+    id_pessoal = cartao_ponto.first().idPessoal_id
+    colaborador = classes.Colaborador(id_pessoal)
+    admissao = colaborador.dados_profissionais.data_admissao
+    demissao = colaborador.dados_profissionais.data_demissao
+    vale_transporte = colaborador.salarios.salarios.ValeTransporte
+
+    sabados, domingos, feriados = obter_datas_nao_uteis_mes(
+        primeiro_dia
+    )
+
+    tipos_ausencia = {
+        "FERIADO": feriados,
+        "SABADO": sabados,
+        "DOMINGO": domingos,
+    }
+
+    nao_uteis = set(sabados + domingos + feriados)
+
+    for cartao in cartao_ponto:
+        dia = cartao.Dia
+        vinculo_ativo = admissao <= dia and(not demissao or dia <= demissao)
+
+        if not vinculo_ativo:
+            continue
+
+        if dia_em_ferias(dia, id_pessoal):
+            continue
+
+        if cartao.Alteracao in {"APP-PONTO", "MANUAL"}:
+            continue
+
+        if aplicar_ausencia_dias_nao_util(cartao, dia, tipos_ausencia):
+            print(f"nao util {dia}")
+            continue
+
+        print(f"passei {dia}")
+        processar_dia_util(cartao, dia, nao_uteis, data_implementacao, hoje, vale_transporte)
+
+    return cartao_ponto
